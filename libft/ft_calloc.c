90d6302e158f537a55b5a3252262d27f0b0ba6e1
atucci <alessiotucci33@gmail.com>
2023-06-13 15:38:38 +0200

    calloc and strdup now works on linux

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stddef.h>
+#include <stdint.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
